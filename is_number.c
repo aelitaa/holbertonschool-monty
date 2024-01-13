27e67e11c0b17cad858acda5dabe9d823b243787
fammad <fred.mmov@gmail.com>
2024-01-13 00:55:43 -0800

    Reason to hate betty

--- is_number.c
+++ is_number.c
@@ -13,7 +13,7 @@ int is_number(char *str)
 
 	if (str == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	while (str[i])
@@ -24,8 +24,8 @@ int is_number(char *str)
 			continue;
 		}
 		if (str[i] < '0' || str[i] > '9')
-			return(0);
+			return (0);
 		i++;
 	}
-	return(1);
+	return (1);
 }
--- run_instruction.c
+++ run_instruction.c
@@ -7,21 +7,23 @@
 void run_instruction(void)
 {
 	stack_t *stack = NULL;
-if (arguments == NULL) {
-	fprintf(stderr, "Arguments is NULL.\n");
-	exit(EXIT_FAILURE);
-}
+	if (arguments == NULL)
+	{
+		fprintf(stderr, "Arguments is NULL.\n");
+		exit(EXIT_FAILURE);
+	}
 
-if (arguments->instruction == NULL) {
-	fprintf(stderr, "Instruction is NULL.\n");
-	exit(EXIT_FAILURE);
-}
+	if (arguments->instruction == NULL)
+	{
+		fprintf(stderr, "Instruction is NULL.\n");
+		exit(EXIT_FAILURE);
+	}
 
-if (arguments->instruction->f == NULL) {
-	fprintf(stderr, "Function pointer null\n");
-	exit(EXIT_FAILURE);
-}
-
+	if (arguments->instruction->f == NULL)
+	{
+		fprintf(stderr, "Function pointer null\n");
+		exit(EXIT_FAILURE);
+	}
 
 	if (arguments->n_tokens == 0)
 		return;
