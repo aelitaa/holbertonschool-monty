74b47a471093430986e3e5d4ed5e9ac79767c0a0
aelitaa <gunel.sultanova@outlook.com>
2024-01-12 23:52:24 -0800

    space

--- invalid_instruction.c
+++ invalid_instruction.c
@@ -6,10 +6,9 @@
 
 void invalid_instruction(void)
 {
-  	dprintf(2, "L%d:unknown instruction %s\n", arguments->line_number, arguments->tokens[0]);
+  	dprintf(2, "L%d: unknown instruction %s\n", arguments->line_number, arguments->tokens[0]);
 	close_stream();
 	free_tokens();
 	free_arguments();
 	exit(EXIT_FAILURE);
 }
-
